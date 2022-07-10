2fc9d1627a7bbf967d6f2f85fb8d4232471849ef
Miles Watson <contact@mileswatson.net>
2022-07-10 20:42:10 +0100

    Added simple in-memory kv store.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, sync::RwLock};
+
+use rocket::{http::Status, State};
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/<key>")]
+async fn get(key: &str, map: &State<RwLock<HashMap<String, String>>>) -> Result<String, Status> {
+    map.read()
+        .map_err(|_| Status::InternalServerError)
+        .and_then(|m| m.get(key).cloned().ok_or(Status::NotFound))
+}
+
+#[post("/<key>", data = "<value>")]
+fn set(key: String, value: String, map: &State<RwLock<HashMap<String, String>>>) -> Status {
+    map.write()
+        .map(|mut m| {
+            m.insert(key, value);
+            Status::Ok
+        })
+        .unwrap_or(Status::InternalServerError)
+}
+
+#[launch]
+fn rocket() -> _ {
+    let map = RwLock::new(HashMap::<String, String>::new());
+    rocket::build().manage(map).mount("/", routes![get, set])
 }
